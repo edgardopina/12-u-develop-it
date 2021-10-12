129e7a3d9ff832081badaac6b5c81d7eaa400757
edgardopina <edgardopina@yahoo.com>
2021-10-12 13:06:44 -0600

    Add db.query to delete one candidate.

--- server.js
+++ server.js
@@ -9,33 +9,61 @@ app.use(express.json());
 
 // connect to database - step 2
 const db = mysql.createConnection(
-	{
-		host: 'localhost', // database host
-		user: 'root', // MySQL username
-		password: 'MyGoofyStrongPassword09()', // MySQL password
-		database: 'election', // database name
-	},
-	console.log(`Connected to the 'election' database`)
+   {
+      host: 'localhost', // database host
+      user: 'root', // MySQL username
+      password: 'MyGoofyStrongPassword09()', // MySQL password
+      database: 'election', // database name
+   },
+   console.log(`Connected to the 'election' database`)
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-	console.log(rows);
+// GET list of all potential candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+// 	console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 111`, (err, row) => {
+// 	if (err) {
+// 		console.log(`~ err`, err);
+// 	}
+// 	console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+// 	if (err) {
+// 		console.log(err);
+// 	}
+// 	console.log(result);
+// });
+
+// Create a candidate; NOTE that we are NOT using id because of its AUTO_INCREMENT property
+const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+					VALUES (?,?,?)`;
+const params = ['Ronald', 'Firbank', 1];
+db.query(sql, params, (err, result) => {
+   if (err) {
+      console.log(err);
+   }
+   console.log(result);
 });
 
-// test connection
-app.get('/', (req, res) => {
-	res.json({
-		message: 'Hello World',
-	});
-});
+//***************** test connection ***************************
+// app.get('/', (req, res) => {
+// 	res.json({
+// 		message: 'Hello World',
+// 	});
+// });
 
 // Default response for any other request (Not Found). THIS IS A CATCHALL ROUTE AND IT
 // MUST BE PLACED AS THE LAST ROUTE to prevent overriding all other GET routes
 app.use((req, res) => {
-	res.status(404).end();
+   res.status(404).end();
 });
 
 // server listener
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+   console.log(`Server running on port ${PORT}`);
 });

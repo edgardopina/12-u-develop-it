7b2b5b3deeda2df5ec51b2561d7a83e2bfd3f08a
edgardopina <edgardopina@yahoo.com>
2021-10-13 11:40:55 -0600

    Add LEFT JOIN statement to GET api routes.

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ const db = mysql.createConnection(
 
 // GET list of all potential candidates - /api/candidates api endpoint
 app.get('/api/candidates', (req, res) => {
-   const sql = `SELECT * FROM candidates`;
+   const sql = `SELECT 
+                  candidates.*,
+                  parties.name AS party_name
+                  FROM candidates
+                  LEFT JOIN parties
+                  ON candidates.party_id = parties.id`;
    db.query(sql, (err, rows) => {
       if (err) {
          // 500 status code - server errors
@@ -37,7 +42,13 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate - /api/candidate/:id - api endpoint
 app.get('/api/candidate/:id', (req, res) => {
-   const sql = `SELECT * FROM candidates WHERE id = ?`;
+   const sql = `SELECT 
+                  candidates.*,
+                  parties.name AS party_name
+                  FROM candidates
+                  LEFT JOIN parties
+                  ON candidates.party_id = parties.id
+                  WHERE id = ?`;
    const params = [req.params.id];
    db.query(sql, params, (err, row) => {
       if (err) {

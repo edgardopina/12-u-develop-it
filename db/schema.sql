9c564db29091ad7dbadaaf398a8d587f01a4158e
edgardopina <edgardopina@yahoo.com>
2021-10-13 21:05:00 -0600

    Added votes table with constraints and tested constraints

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
@@ -24,3 +25,19 @@ CREATE TABLE voters (
  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+voter_id INTEGER NOT NULL,
+candidate_id INTEGER NOT NULL,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+/* specifies that the values inserted into the voter_id field must be unique. For example, 
+   whoever has a voter_id of 1 can only appear in this table once */
+CONSTRAINT uc_voter UNIQUE (voter_id),
+/* relates this table votes through voter_id to the table voters through id.
+   DELETE CASCADE indicates that deleting the reference key (in voters) will also delete the 
+   entire row from this table */
+CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+/* Similart as above   */
+CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+);
